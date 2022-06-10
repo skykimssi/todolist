d343fc778ead437e4dfdda505cb5366256e3b598
smartflat-kimsk <102001812+smartflat-kimsk@users.noreply.github.com>
2022-06-10 17:09:31 +0900

    메모 세팅

--- src/router/memo.js
+++ src/router/memo.js
@@ -1,9 +1,40 @@
+import { useState, useEffect } from "react";
+import MemoIntro from '../component/memo/MemoIntro';
+
+
+
 function Memo() {
+    const [memoCnt, setMemoCnt] = useState(0);
+    const [memoData, setMemoData] = useState(null);
+
+    useEffect(()=>{
+        console.log("첫실행~")
+    },[])
+
     return (
         <div className="Memo">
-            <div>얘는 메모 초기 페이지</div>
+            {memoCnt === 0?<MemoIntro onClick={addMemoBtn} />:""}
+            
+            
+                
+            
             
         </div>
     )
+
+    
+    function addMemoBtn() {
+        //신규 메모 정보 세팅
+        //추가될 메모 정보 추가
+        const memoIdx = 0;
+        localStorage.getItem("memoData");
+        localStorage.setItem("memoData",memoData);
+        
+        setMemoCnt((precnt)=>precnt+1);
+        
+    }
+
 }
+
+
 export default Memo;
