c0220ea86b5120deff19a24949cdf86d98c4f9b9
skykimssi <53454388+skykimssi@users.noreply.github.com>
2022-06-20 08:04:38 +0900

    20220620

--- src/router/home.js
+++ src/router/home.js
@@ -1,12 +1,24 @@
+import "./Home.css";
+
 function Home() {
     return (
         <div className="Home">
             {/* 네비 메뉴 들어감 */}
-            <h1>여기는 초기 페이지</h1>
-            <div>소개 1</div>
-            <div>소개 2</div>
-            <div>소개 3</div>
-            <div>소개 4</div>
+            <div className="mainTopArea">
+                <div className="mainTop_textArea">
+                    <h1>개발자입니다</h1>
+                    <div>대충 인사말 들어갈 자리!</div>
+                    <div>대충 소개 들어갈 자리 입니다.</div>
+                </div>
+            </div>
+            <div className="mainArea2">
+                <div className="item">
+                    여기에 이미지
+                </div>
+                <div className="item">
+                    여기에는 대충 내 소개 들어감 이름 등등등
+                </div>
+            </div>
         </div>
         
     );
